b3d3dfd94c83891b827406823b4743558277222e
Navadeep Jakkamsetti <navadeepjakkamsetti26@gmail.com>
2025-07-01 19:22:08 +0530

    Main web page

--- crop_web.py
+++ crop_web.py
@@ -49,12 +49,15 @@ elif app_mode == "About":
                 This dataset is recreated using offline augmentation from the original dataset.The original dataset can be found on this github repo.
                 This dataset consists of about 87K rgb images of healthy and diseased crop leaves which is categorized into 38 different classes.The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure.
                 A new directory containing 33 test images is created later for prediction purpose.
-                ## About the Plant Disease Recognition System
-                This project aims to assist in the early detection of diseases in crop leaves by leveraging deep learning models. Early detection of plant diseases can significantly impact agricultural productivity, enabling farmers to take preventive measures before the diseases spread.
+                
+                ### About the Plant Disease Recognition System
+                This web application aims to assist in the early detection of diseases in crop leaves by leveraging deep learning models. 
+                
                 ### Purpose
                 * This system uses advanced image recognition techniques to classify plant leaves as either healthy or diseased, providing a user-friendly interface where farmers and researchers can upload leaf images for diagnosis. 
                 
                 * The model used here is based on TensorFlow and has been trained to accurately identify a variety of diseases across different types of crops.
+                
                 ### Content
                 The images have been split into training, validation, and test sets using an 80/20 distribution while maintaining the original directory structure of the dataset:
                 
